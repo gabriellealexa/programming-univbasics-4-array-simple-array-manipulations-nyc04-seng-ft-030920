c377672f95ab1da546a9514cead5e2a8ee2967bc
Gabrielle Noel <gabby@gabriellealexa.com>
2020-02-27 04:53:54 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,45 @@
+def using_push(colors_in_the_rainbow, next_color)
+ return colors_in_the_rainbow.push(next_color)
+end
+
+def using_unshift(boroughs_in_nyc, new_neighborhood)
+  return boroughs_in_nyc.unshift(new_neighborhood)
+end
+
+def using_pop(array)
+  return array.pop
+end
+
+def pop_with_args(array)
+  return array.pop(2)
+end
+
+def using_shift(array)
+  return array.shift 
+end
+
+def shift_with_args(array)
+  return array.shift(2)
+end
+
+def using_concat(array1, array2)
+  return array1.concat(array2)
+end
+
+def using uniq(array)
+  return array.uniq
+end 
+
+def using_flatten(array)
+  return array.flatten
+end
+
+def using_delete(array, string)
+  return array.delete(string)
+end
+
+def using_delete_at(array, integer)
+  return array.delete(integer)
+end
+
+  
