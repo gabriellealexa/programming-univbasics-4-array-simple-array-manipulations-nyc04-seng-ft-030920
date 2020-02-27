4b117c8f37bcd7f334b99f48ae272010dcf9af95
Gabrielle Noel <gabby@gabriellealexa.com>
2020-02-27 05:02:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,7 +27,7 @@ def using_concat(array1, array2)
 end
 
 def using_insert(array, element)
-  return array.insert(4)
+  return array.insert(element)
 end
 
 def using_uniq(array)
@@ -42,6 +42,6 @@ def using_delete(array, string)
   return array.delete(string)
 end
 
-def using_delete_at(famous_robots, integer)
+def using_delete_at(array, integer)
   return array.delete(integer)
 end
