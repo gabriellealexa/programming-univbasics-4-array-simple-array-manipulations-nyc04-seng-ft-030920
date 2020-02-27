7112ad267b258ce8ff892fdda429041f8454b6e1
Gabrielle Noel <gabby@gabriellealexa.com>
2020-02-27 04:56:36 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -26,6 +26,9 @@ def using_concat(array1, array2)
   return array1.concat(array2)
 end
 
+def using_insert(array, element)
+  return array.insert()
+
 def using_uniq(array)
   return array.uniq
 end 
